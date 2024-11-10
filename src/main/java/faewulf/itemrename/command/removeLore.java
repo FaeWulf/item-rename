a1b357ff789108ace0fd99f506207395a8efac61
FaeWulfs <ngolamaz3@gmail.com>
2024-11-10 16:27:23 +0700

    # Fix: removelore not remove the lore component from the item.

--- src/main/java/faewulf/itemrename/command/removeLore.java
+++ src/main/java/faewulf/itemrename/command/removeLore.java
@@ -1,14 +1,11 @@
 package faewulf.itemrename.command;
 
 import com.mojang.brigadier.CommandDispatcher;
-import com.mojang.brigadier.arguments.StringArgumentType;
 import com.mojang.brigadier.context.CommandContext;
 import com.mojang.brigadier.exceptions.CommandSyntaxException;
 import com.mojang.brigadier.exceptions.SimpleCommandExceptionType;
-import faewulf.itemrename.util.loreEditor;
 import faewulf.itemrename.util.ownerCheck;
 import faewulf.itemrename.util.permission;
-import faewulf.itemrename.util.stringParser;
 import me.lucko.fabric.api.permissions.v0.Permissions;
 import net.minecraft.component.DataComponentTypes;
 import net.minecraft.item.ItemStack;
@@ -16,10 +13,7 @@ import net.minecraft.server.command.CommandManager;
 import net.minecraft.server.command.ServerCommandSource;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.text.Text;
-import net.minecraft.util.Formatting;
 import net.minecraft.util.Hand;
-
-import java.util.Objects;
 
 public class removeLore {
 
@@ -47,7 +41,7 @@ public class removeLore {
 
         ownerCheck.check(player, holding);
 
-        holding.remove(DataComponentTypes.LORE);
+        holding.set(DataComponentTypes.LORE, holding.getDefaultComponents().get(DataComponentTypes.LORE));
 
         return 0;
     }
