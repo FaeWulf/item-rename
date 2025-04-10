e02f4941e53d6a06056351d3105c42101ae0025e
FaeWulfs <ngolamaz3@gmail.com>
2025-04-10 12:01:59 +0700

    + Add: Proper handling custom color from hex code.

---
+++ src/main/java/faewulf/itemrename/inter/ICustomStyle.java
@@ -0,0 +1,8 @@
+package faewulf.itemrename.inter;
+
+import net.minecraft.text.Style;
+import net.minecraft.util.Formatting;
+
+public interface ICustomStyle {
+    public Style ItemRename$withCustomFormatting(CustomFormatting... formattings);
+}
